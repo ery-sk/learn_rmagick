a3d25767ac2a53fc86467ada4f9df791824dcebb
Ryosuke Hiroe <ride.poke@gmail.com>
2019-06-13 17:44:20 +0900

    .

--- README.md
+++ README.md
@@ -92,3 +92,18 @@ end
 ```
 
 ## 取得した画像を規定の場所に重ねて合成して保存する
+```rb
+# images_controller.rb
+class ImagesController < ApplicationController
+  def new; end
+
+  def create
+    def create
+      base_image = Magick::Image.from_blob(params[:image_1].read).first
+      addition_image = Magick::Image.from_blob(params[:image_2].read).first
+      image = base_image.composite(addition_image, 0, 0, Magick::OverCompositeOp)
+      image.write('public/pile_up.png')
+    end
+  end
+end
+```
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,9 @@ class ImagesController < ApplicationController
   def new; end
 
   def create
-    images = Magick::ImageList.new(params[:image_1].tempfile.path, params[:image_2].tempfile.path)
-    images.append(false).write('public/join.png')
+    base_image = Magick::Image.from_blob(params[:image_1].read).first
+    addition_image = Magick::Image.from_blob(params[:image_2].read).first
+    image = base_image.composite(addition_image, 0, 0, Magick::OverCompositeOp)
+    image.write('public/pile_up.png')
   end
 end
